1907362037aaf948ac8890da423020423576274a
Josh <joshuamorisonhalliwell@gmail.com>
2021-10-13 14:51:01 +1300

    Procedural Generation
    Able to pick how long the level is

---
+++ Assets/Josh Scripts/ProcGen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcGen : MonoBehaviour
+{
+    [SerializeField] int width;
+    [SerializeField] GameObject GroundLevel;
+    void Start()
+    {
+        Generation();
+    }
+
+    // Update is called once per frame
+    void Generation()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            Instantiate(GroundLevel, new Vector2(x, 0), Quaternion.identity);
+        }
+        
+    }
+}
---
+++ Assets/Josh Scripts/ProcGenUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcGenUp : MonoBehaviour
+{
+    [SerializeField] int width;
+    [SerializeField] GameObject UpLevel;
+    void Start()
+    {
+        Generation();
+    }
+
+    // Update is called once per frame
+    void Generation()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            Instantiate(UpLevel, new Vector2(x, 4), Quaternion.identity);
+        }
+
+    }
+}
