0eb5f1ce80610245491e938f88da7ecf8d4910d8
Phoenix <phoenix.millar.020@gmail.com>
2021-10-13 14:25:01 +1300

    Movement Script + Jpeg of Bad Guy
    scripting for the movement and jpeg

---
+++ Assets/Script/moveMent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveMent : MonoBehaviour
+{
+    float distance = 4;
+
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + distance);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - distance);
+        }
+        transform.position += transform.right * speed * Time.deltaTime;
+    }
+}
